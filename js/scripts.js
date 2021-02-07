8e86d46425dc7a23caf2ab9cb8daab2c5bcd96dc
Sergey <sergeynechiporenko91@gmail.com>
2021-02-07 21:39:39 +0200

    added all

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function () {
+	$('.mini-menu').click(function () {
+		if ($('nav ul').css('display') == 'none') {
+			$('nav ul').slideDown();
+		} else {
+			$('nav ul').slideUp();
+		}
+	});
+
+	$('.service-slider').slick({
+		arrows: false,
+		dots: true,
+		responsive: [{
+			breakpoint: 600,
+			settings: {
+				dots: false,
+				centerMode: true,
+				centerPadding: '50px',
+			} 
+		}]
+	});
+	AOS.init();
+});
+
+let mask = document.querySelector('.mask');
+
+window.addEventListener('load', () => {
+	mask.classList.add('hide');
+	setTimeout(() => {
+		mask.remove();
+	 }, 600);
+});
+
